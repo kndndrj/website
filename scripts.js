e7ce0d1ae90df2bc6013b903563183b6c9bfdf52
Andrej Kenda <andrej.baca14@gmail.com>
2021-06-16 08:37:17 +0200

    xmr addr change

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 function openPopup(itemToShow) {
     var popup = document.querySelector(itemToShow);
     if (popup.style.display === "block") {
-      popup.style.display = "none";
+        popup.style.display = "none";
     } else {
-      popup.style.display = "block";
+        popup.style.display = "block";
     }
 }
