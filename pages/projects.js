7b52dc58cd5db588e6d596e94af321f90b64258a
Andrej Kenda <andrej.baca14@gmail.com>
2022-12-18 18:55:13 +0100

    dynamically fetch projects from github

--- pages/projects.js
+++ pages/projects.js
@@ -1,43 +1,50 @@
-import Layout from "../components/layout"
-import { getAllProjectIds, getProjectData } from "../lib/projects"
-import Head from "next/head"
-import utilStyles from "../styles/utils.module.css"
-import A from "../components/a"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState, useEffect } from 'react';
+import Layout from "../components/layout";
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+import A from "../components/a";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Projects({ contents }) {
+export default function ProjectsPage() {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://api.github.com/users/kndndrj/repos")
+      .then((res) => {
+        if (res.status != 200) {
+          return [];
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <Layout>
       <Head>
         <title>Projekti</title>
       </Head>
-      <section className={utilStyles.markdown}>
-        {contents.map(({ id, title, url, contentHtml }) => (
-          <article id={id} key={id}>
-            <h2 className={utilStyles.headingSubtitle}>
-              {title}
-            </h2>
-            <small className={utilStyles.lightText}>
-              <A href={url} blank><FontAwesomeIcon icon={["fab", "github"]} />Git</A>
-            </small>
-            <div className={utilStyles.markdownHtmlContents} dangerouslySetInnerHTML={{ __html: contentHtml }} />
-          </article>
-        ))}
+      <section className={utilStyles.headingDescription}>
+        {isLoading && <p>Loading...</p>}
+
+        {data &&
+          <ul className={utilStyles.list}>
+            {data.map((params) =>
+              <>
+                {params.fork ||
+                  <li className={utilStyles.listItem} key={params.name}>
+                    <A href={"/projects/" + params.name}><FontAwesomeIcon icon={["fas", "hammer"]} />{params.name}</A>
+                  </li>
+                }
+              </>
+            )}
+          </ul>}
       </section>
     </Layout>
-  )
-}
-
-export async function getStaticProps() {
-  const projects = getAllProjectIds()
-  let contents = []
-  for (let project of projects) {
-    let content = await getProjectData(project.params.id)
-    contents.push(content)
-  };
-  return {
-    props: {
-      contents
-    }
-  }
+  );
 }
