cb521e101e00a68b78309215998c90b6777c76e4
Andrej Kenda <andrej.baca14@gmail.com>
2023-01-03 12:59:56 +0100

    added more icon

--- components/md.tsx
+++ components/md.tsx
@@ -13,7 +13,11 @@ export default function Md({ markdown, buttonFunc }: MdProps) {
 		<div key={markdown.id}>
 			<h2 className={styles.title}>
 				{buttonFunc && (
-					<button className={styles.button} onClick={buttonFunc}>More</button>
+					<button className={styles.button} onClick={buttonFunc}>
+						<svg width="20" height="20" fill="currentColor" viewBox="0 0 15 16">
+							<path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+						</svg>
+					</button>
 				)}
 				{markdown.title}
 			</h2>
